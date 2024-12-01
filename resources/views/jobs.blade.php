a8d6ab35dcfc80443ce71b63c25365cc7fb42adb
Samuel  Suarez <samuel.suarez@linktic.com>
2024-12-01 08:12:02 -0500

    Tailwind css?

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,9 +5,19 @@
 
  </x-slot:heading> 
 
+<section class="w-100 h-100 gap-10 flex justify-center items-center flex-wrap text-wrap text-center">
 @foreach($jobs as $job) 
+
+<div class="rounded text-wrap w-[400px]" style="border: 1px solid gray; box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);">
+    <h3>{{$job['title'] }} : {{$job['salary']}}</h3>
       <a href="/job/{{$job['id']}}">
-      <li> {{$job['title'] }}: {{$job['salary'] }}  </li>
+              <div>
+                Ver mas
+              </div>
       </a>
+</div>
+
  @endforeach
+</section>
+
  </x-layout>
